06ed7a70fdee0edef79fba777e1cbac6e67445ca
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-03-14 08:37:20 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE  || 'files_manager';
+    const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}/${database}`;
     this.client = new MongoClient(url, { useUnifiedTopology: true });
     this.connect();
@@ -18,7 +18,7 @@ class DBClient {
       console.error('MongoDB connection error:', err);
     }
   }
-  
+
   isAlive() {
     return !!this.client && this.client.isConnected();
   }
